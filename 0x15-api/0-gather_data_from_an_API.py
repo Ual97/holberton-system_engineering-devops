410f016b9b8c9041201d6c0c82008e43baec9306
Ual97 <3951@holbertonschool.com>
2022-05-16 13:42:04 -0700

    changed varname to comply with pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,25 +4,33 @@
 import requests
 from sys import argv
 
-user = requests.get("https://jsonplaceholder.typicode.com/users")
-for i in user.json():
-    if i.get('id') == int(argv[1]):
-        EMPLOYEE_NAME = (i.get('name'))
-        break
-NUMBER_OF_DONE_TASKS = 0
-TOTAL_NUMBER_OF_TASKS = 0
-TASK_TITLE = []
-todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-for j in todos.json():
-    if j.get('userId') == int(argv[1]):
-        if j.get('completed') is True:
-            NUMBER_OF_DONE_TASKS += 1
-            TASK_TITLE.append(j.get('title'))
-            TOTAL_NUMBER_OF_TASKS += 1
-        else:
-            TOTAL_NUMBER_OF_TASKS += 1
 
-print("Employee {} is done with tasks ({}/{}):".format(EMPLOYEE_NAME,
-      NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-for h in TASK_TITLE:
-    print("\t {}".format(h))
+def func():
+    """function to manage info from API's"""
+    user = requests.get("https://jsonplaceholder.typicode.com/users")
+    for i in user.json():
+        if i.get('id') == int(argv[1]):
+            EMPLOYEE_NAME = (i.get('name'))
+            break
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_TASKS = 0
+    TASK_TITLE = []
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    for j in todos.json():
+        if j.get('userId') == int(argv[1]):
+            if j.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(j.get('title'))
+                TOTAL_TASKS += 1
+            else:
+                TOTAL_TASKS += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_TASKS))
+    for h in TASK_TITLE:
+        print("\t {}".format(h))
+
+
+if __name__ == "__main__":
+    func()
