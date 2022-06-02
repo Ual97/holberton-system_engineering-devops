2fc8da7ad288d9733c065a6a6ea88322c575f47c
Ual97 <3951@holbertonschool.com>
2022-06-01 19:13:12 -0700

    ex 0 done

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#using strace to find out why apache givi error 500
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+README for thirs debugging pruject
